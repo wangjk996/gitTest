eca6e032aab68a6632d5383b09c75f884b05d897
test <test@qq.com>
2020-04-14 17:22:35 +0800

    添加一个Student类

---
+++ src/cn/test/demo/Student.java
@@ -0,0 +1,30 @@
+package cn.test.demo;
+
+public class Student {
+    private String usermame;
+    private String sex;
+
+    public String getUsermame() {
+        return usermame;
+    }
+
+    public void setUsermame(String usermame) {
+        this.usermame = usermame;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "usermame='" + usermame + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
